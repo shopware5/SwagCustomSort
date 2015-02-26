c834ffb93ddc4d1c4826d0b3fc410fae3b029c44
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2015-02-26 17:45:17 +0200

    Add backend controller

---
+++ Controllers/Backend/CustomSort.php
@@ -0,0 +1,6 @@
+<?php
+
+class Shopware_Controllers_Backend_CustomSort extends Shopware_Controllers_Backend_Application
+{
+
+}
--- Subscriber/ControllerPath.php
+++ Subscriber/ControllerPath.php
@@ -16,6 +16,23 @@ class ControllerPath implements SubscriberInterface
 
     public static function getSubscribedEvents()
     {
+        return array(
+            'Enlight_Controller_Dispatcher_ControllerPath_Backend_CustomSort' => 'onGetCustomSortControllerPath'
+        );
+    }
+
+    /**
+    * This function is responsible to resolve the backend / frontend controller path.
+    *
+    * @param  \Enlight_Event_EventArgs $args
+    * @return string
+    */
+    public function onGetCustomSortControllerPath(\Enlight_Event_EventArgs $args)
+    {
+        switch ($args->getName()) {
+            case 'Enlight_Controller_Dispatcher_ControllerPath_Backend_CustomSort':
+                return $this->bootstrap->Path() . 'Controllers/Backend/CustomSort.php';
+        }
     }
 
 }
