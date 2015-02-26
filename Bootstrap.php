994830724e4bd9133356ebaf2738fde175b012c2
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2015-02-26 15:01:00 +0200

    Added base elements

---
+++ Bootstrap.php
@@ -0,0 +1,73 @@
+<?php
+
+class Shopware_Plugins_Frontend_SwagCustomSort_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+{
+    /**
+     * Returns the plugin label which is displayed in the plugin information and
+     * in the Plugin Manager.
+     * @return string
+     */
+    public function getLabel()
+    {
+        return 'Custom sorting';
+    }
+
+    /**
+     * Returns the version of the plugin as a string
+     *
+     * @return string|void
+     * @throws Exception
+     */
+    public function getVersion()
+    {
+        $info = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'plugin.json'), true);
+
+        if ($info) {
+            return $info['currentVersion'];
+        } else {
+            throw new Exception('The plugin has an invalid version file.');
+        }
+    }
+
+    /**
+     * Install Plugin / Add Events
+     *
+     * @throws Enlight_Exception
+     * @return bool
+     */
+    public function install()
+    {
+        $this->subscribeEvents();
+
+        return array('success' => true, 'invalidateCache' => array('backend'));
+    }
+
+    /**
+     * Registers all necessary events.
+     */
+    public function subscribeEvents()
+    {
+
+    }
+
+    /**
+     * Creates the backend menu item.
+     */
+    public function createMenu()
+    {
+        $parent = $this->Menu()->findOneBy(array('label' => 'Items'));
+
+        $this->createMenuItem(
+            array(
+                'label' => 'Custom sort',
+                'controller' => 'Custom sort',
+                'action' => 'Index',
+                'active' => 1,
+                'class' => 'sprite-blue-document-text-image',
+                'parent' => $parent,
+                'position' => 6,
+            )
+        );
+    }
+
+}
