0898b2c262f804a1710db47746a0aea09392e58c
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2015-02-26 14:41:40 +0200

    Init

---
+++ Boostrap.php
@@ -0,0 +1,15 @@
+<?php
+
+class Shopware_Plugins_Frontend_SwagCustomSort_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+{
+    /**
+     * Returns the plugin label which is displayed in the plugin information and
+     * in the Plugin Manager.
+     * @return string
+     */
+    public function getLabel()
+    {
+        return 'Custom sorting';
+    }
+
+}
