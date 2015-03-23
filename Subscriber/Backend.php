9f6c7c59a23174c2cc129602b42d571e200008de
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2015-03-23 10:01:19 +0200

    Remove sort on category or product remove

--- Subscriber/Backend.php
+++ Subscriber/Backend.php
@@ -16,7 +16,9 @@ class Backend implements SubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            'Enlight_Controller_Action_PostDispatchSecure_Backend_Index' => 'onPostDispatchSecureBackendIndex'
+            'Enlight_Controller_Action_PostDispatchSecure_Backend_Index' => 'onPostDispatchSecureBackendIndex',
+            'Shopware\Models\Article\Article::preRemove' => 'preRemoveArticle',
+            'Shopware\Models\Category\Category::preRemove' => 'preRemoveCategory'
         );
     }
 
@@ -32,4 +34,31 @@ class Backend implements SubscriberInterface
         $view->addTemplateDir($this->bootstrap->Path() . 'Views/');
         $view->extendsTemplate('backend/custom_sort/header.tpl');
     }
+
+    public function preRemoveArticle(Enlight_Event_EventArgs $arguments)
+    {
+        $articleModel = $arguments->get('entity');
+        $articleDetailId = $articleModel->getId();
+
+        $builder = Shopware()->Models()->getDBALQueryBuilder();
+        $builder->delete('s_articles_sort')
+            ->where('articleId = :articleId')
+            ->setParameter('articleId', $articleDetailId);
+
+        $builder->execute();
+    }
+
+    public function preRemoveCategory(Enlight_Event_EventArgs $arguments)
+    {
+        $categoryModel = $arguments->get('entity');
+        $categoryId = $categoryModel->getId();
+
+        $builder = Shopware()->Models()->getDBALQueryBuilder();
+        $builder->delete('s_articles_sort')
+            ->where('categoryId = :categoryId')
+            ->setParameter('categoryId', $categoryId);
+
+        $builder->execute();
+    }
+
 }
